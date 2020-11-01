6c6d73c233baa345e20a2f613e70d6599877f67c
Aswin K R <aswinkr77@gmail.com>
2020-11-01 15:52:37 +0530

    new js file

---
+++ 01 Javascript Drumstick/js/javascript.js
@@ -0,0 +1,22 @@
+function removeTransition(e) {
+    if(e.propertyName !== 'transform')
+        return;
+    
+    e.target.classList.remove('on-play');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.keys[data-key="${e.keyCode}"]`);
+
+    if(!audio)
+        return;
+
+    key.classList.add('on-play');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.keys'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
