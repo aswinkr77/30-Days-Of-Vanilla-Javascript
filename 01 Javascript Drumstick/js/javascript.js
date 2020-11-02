2dfd60435104a58e82b795b547620fe4b258eb15
Aswin K R <aswinkr77@gmail.com>
2020-11-03 00:40:51 +0530

    Reverted a change

--- 01 Javascript Drumstick/js/javascript.js
+++ 01 Javascript Drumstick/js/javascript.js
@@ -17,6 +17,18 @@ function playSound(e) {
     audio.play();
 }
 
+function sound(e) {
+    const audio = document.querySelector(`audio[data-key="${e}"]`);
+    const key = document.querySelector(`.keys[data-key="${e}"]`);
+
+    if(!audio)
+        return;
+
+    key.classList.add('on-play');
+    audio.currentTime = 0;
+    audio.play();
+}
+
 const keys = Array.from(document.querySelectorAll('.keys'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
