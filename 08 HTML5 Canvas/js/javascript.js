029226e4c205dfcc332f4e598afa84a8f5b8751c
Aswin K R <aswinkr77@gmail.com>
2020-11-11 23:58:09 +0530

    Day 8

--- 08 HTML5 Canvas/js/javascript.js
+++ 08 HTML5 Canvas/js/javascript.js
@@ -1,5 +1,11 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
+const clr = document.querySelector(".clear");
+const color = document.getElementById("brushColor");
+const brush = document.getElementById("brushSize");
+const download = document.querySelector(".download");
+
+brush.defaultValue = 1;
 
 var canvasW = canvas.offsetWidth;
 var canvasH = canvas.offsetHeight;
@@ -36,9 +42,30 @@ function drawing(e)
 
 canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", () => draw = false);
-canvas.addEventListener("mouseout", () => draw = false );
 
 canvas.addEventListener("mousedown", e => {
     draw = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
 });
+
+function updateColor()
+{
+    context.strokeStyle = `${this.value}`;
+}
+
+function updateBrushSize()
+{
+    context.lineWidth = this.value;
+}
+
+function Downloadimg()
+{
+    var img = canvas.toDataURL("image/jpeg");
+    this.href = img;
+}
+
+clr.addEventListener("click", () => context.clearRect(0, 0, canvas.width, canvas.height));
+color.addEventListener("mousemove", updateColor);
+color.addEventListener("change", updateColor);
+brush.addEventListener("change", updateBrushSize);
+download.addEventListener("click", Downloadimg);
